893e55836f168009ab27d8e3ef8f81bb1bf7c42d
Jansey Scofield <janscofield10@gmail.com>
2024-10-08 22:50:50 -0300

    Adicionando funcionalidade de, ao  criar um usuario novo,  uma senha temporaria seja criada automaticamente

--- src/controllers/UsuarioController.ts
+++ src/controllers/UsuarioController.ts
@@ -2,8 +2,8 @@ import {create} from 'services/UsuarioServices';
 import {Request,Response} from 'express';
 
 function cadastrarUsuario(req : Request, res : Response){
-    create(req.body).then((novoUsuario : any) =>{
-        return res.status(201).send({usuarioCadastrado : novoUsuario});
+    create(req.body).then((dados : any) =>{
+        return res.status(201).send({usuarioCadastrado : dados.usuarioCadastrado, senhaTemporaria : dados.senhaTemporaria});
     }).catch((e : Error) => {
         return res.status(400).send({mensagem : "Erro ao cadastrar um novo usuário: "  + e.message});
     });
--- src/services/UsuarioServices.ts
+++ src/services/UsuarioServices.ts
@@ -3,12 +3,13 @@ import { Usuario } from "models/UsuarioModel";
 const bcrypt = require('bcrypt');
 
 async function create(novoUsuario : any) : Promise<any>{
-    const senhaEncriptografada : string = await bcrypt.hash(novoUsuario.Senha, 10);
+    const senha : string = "usuariolabateq" + Math.random() * 11;
+    const senhaEncriptografada : string = await bcrypt.hash(senha, 10);
     novoUsuario.Senha =  senhaEncriptografada;
     try{
         await database.sync();
-       await Usuario.create(novoUsuario);
-        return novoUsuario;
+        await Usuario.create(novoUsuario);
+        return {usuarioCadastrado : novoUsuario, senhaTemporaria : senha};
     }
     catch(e){
         throw new Error((e as Error).message);
